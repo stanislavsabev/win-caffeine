1418c6ec14a621a9809daeca4b4fced196352495
stanislavsabev <bezraboten.34@gmail.com>
2023-06-02 20:49:39 +0200

    Add mode change.

--- src/win_caffeine.py
+++ src/win_caffeine.py
@@ -1,12 +1,18 @@
 import sys
-import PySide2.QtGui
 
 from win_caffeine import qt
+from win_caffeine import screen_lock
 import qdarktheme
 
+APP_NAME = "win-caffeine"
+ON_OFF_MODE = {
+    True: "off",
+    False: "on",
+}
 
-light_icon_path = "assets/coffee-on.png"
-dark_icon_path = "assets/coffee-on-dark.png"
+
+def get_icon_path(mode, theme) -> str:
+    return f"assets/coffee-{mode}-{theme}.png"
 
 
 def is_dark_theme(palette: qt.QPalette) -> bool:
@@ -15,22 +21,35 @@ def is_dark_theme(palette: qt.QPalette) -> bool:
     return lum > 127
 
 
+def theme_from_palette(palette) -> str:
+    return "dark" if is_dark_theme(palette) else "light"
+
+
 class MainWindow(qt.QMainWindow):
     def __init__(
         self,
+        theme,
         parent: qt.QWidget | None = None,
         flags: qt.Qt.WindowFlags | None = None,
     ) -> None:
         flags = flags or qt.Qt.WindowFlags()
         super().__init__(parent, flags)
+        self.button_icons = {mode: qt.QIcon(get_icon_path(mode, theme)) for mode in ["on", "off"]}
+        self.setup_ui()
+
+    def setup_ui(self):
         central_widget = qt.QWidget()
         central_layout = qt.QVBoxLayout()
 
-        icon_path = dark_icon_path if is_dark_theme(self.palette()) else light_icon_path
-        toggle_button = qt.QPushButton(qt.QIcon(icon_path), "Toggle")
+        toggle_button = qt.QPushButton()
         toggle_button.setObjectName("toggle_button")
         toggle_button.clicked.connect(self.on_toggle_toggle_button_clicked)
-        
+        self.toggle_button = toggle_button
+        self.update_toggle_button()
+
+        mode_label = qt.QLabel(self.get_state_message())
+        self.mode_label = mode_label
+        central_layout.addWidget(mode_label)
         central_layout.addWidget(toggle_button)
         central_widget.setLayout(central_layout)
         self.setCentralWidget(central_widget)
@@ -38,11 +57,29 @@ class MainWindow(qt.QMainWindow):
     def close(self) -> bool:
         return self.hide()
 
+    def update_toggle_button(self):
+        mode = ON_OFF_MODE[screen_lock.is_on()]
+        next_mode = ON_OFF_MODE[not screen_lock.is_on()]
+        self.toggle_button.setText(f"Turn {next_mode}")
+        self.toggle_button.setIcon(self.button_icons[mode])
+
+    def get_state_message(self) -> str:
+        state = "disabled" if screen_lock.is_on() else "enabled"
+        return f"Screen lock is {state}"
+
     def on_toggle_toggle_button_clicked(self):
-        print('clicked')
+        action = screen_lock.prevent_screen_lock
+        if screen_lock.is_on():
+            action = screen_lock.release_screen_lock
+        try:
+            action()
+        except Exception:
+            self.statusBar().showMessage("Screen lock action failed!", 2)
+        finally:
+            self.mode_label.setText(self.get_state_message())
 
 
-def main():
+def gui(*args, **kwargs):
     # Enable HiDPI.
     qdarktheme.enable_hi_dpi()
 
@@ -50,12 +87,18 @@ def main():
     app = qt.QApplication([])
     qdarktheme.setup_theme("auto", default_theme="light")
 
+    mode = ON_OFF_MODE[screen_lock.is_on()]
+    theme = theme_from_palette(app.palette())
+    icon_path = get_icon_path(mode, theme)
+
     # Create the main window
-    window = MainWindow()
+    window = MainWindow(theme=theme)
+    window.setWindowTitle(APP_NAME)
+    window.setWindowIcon(qt.QIcon(icon_path))
+
     # Create the system tray icon
-    icon_path = dark_icon_path if is_dark_theme(app.palette()) else light_icon_path
     tray_icon = qt.QSystemTrayIcon(qt.QIcon(icon_path), parent=app)
-    tray_icon.setToolTip("App Name")
+    tray_icon.setToolTip(APP_NAME)
 
     # Create the system tray menu
     tray_menu = qt.QMenu()
@@ -63,9 +106,8 @@ def main():
     tray_menu.addAction("Exit", app.quit)  # Quit the application
     tray_icon.setContextMenu(tray_menu)
 
-    # Show the main window (optional)
-    if window is not None:
-        window.show()
+    # Show the main window
+    window.show()
 
     # Minimize to system tray
     tray_icon.show()
@@ -73,6 +115,21 @@ def main():
 
     # Start the application event loop
     app.exec_()
+
+
+def cmd(*args, **kwargs):
+    pass
+
+
+def main():
+    # TODO: parse args
+
+    # clear existing state
+    screen_lock.release_screen_lock()
+
+    # choose UI or cmd
+    args = []
+    gui(args)
 
 
 if __name__ == "__main__":
---
+++ src/win_caffeine/screen_lock.py
@@ -0,0 +1,48 @@
+import ctypes
+import time
+
+# Define constants
+ES_CONTINUOUS = 0x80000000
+ES_SYSTEM_REQUIRED = 0x00000001
+
+
+class _State:
+    is_on = True
+    time_span_min = 0
+    refresh_interval_sec = 0
+
+
+def is_on() -> bool:
+    """True if screen lock is NOT prevented."""
+    return _state.is_on
+
+
+def prevent_screen_lock():
+    """Prevent screen lock."""
+    ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
+    _state.is_on = False
+
+
+def release_screen_lock():
+    """Release screen lock prevention."""
+    ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
+    _state.is_on = True
+
+
+def run_prevent_screen_lock(time_span_min: int, refresh_interval_sec: int = 30):
+    """Prevent screen lock for amount of time.
+
+    Args:
+        time_span_min (int): Number of minutes, screen lock prevention to last.
+        refresh_interval_sec (int): Number of seconds after which
+            screen lock prevention is repeated. Default is 30 sec.
+    """
+    end_time = time.time() + (time_span_min * 60)
+
+    while time.time() < end_time:
+        prevent_screen_lock()
+        time.sleep(refresh_interval_sec)
+    release_screen_lock()
+
+
+_state = _State()
